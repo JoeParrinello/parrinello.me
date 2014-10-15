1014d1c2c7d9da5ce6ec572a185c1c6f1ef4633b
Joe Parrinello <joe@hardorange.org>
2014-10-15 17:38:42 -0400

    Just Doing it without secret

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 var sys = require('sys');
 var exec = require('child_process').exec;
+var secrets = require('./config/secrets.js');
+
 
 var port = process.env.PORT || 80;
 
@@ -14,12 +16,6 @@ app.use(methodOverride('X-HTTP-Method-Override'));
 app.use(express.static(__dirname + '/public'));
 
 app.post('/gitpull', function(req, res) {
-  if(req.query.secret_key != config.secret_key) {
-      util.log("[warning] Unauthorized request " + req.url);
-      res.writeHead(401, "Not Authorized", {'Content-Type': 'text/html'});
-      res.end('401 - Not Authorized');
-      return;
-  }
   res.end();
   function puts(error, stdout, stderr) {sys.puts(stdout)}
   exec("git reset --hard HEAD", puts);
