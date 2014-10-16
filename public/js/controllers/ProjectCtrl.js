525704ca50ed06263701846c10f49b9a6cbff589
Joe Parrinello <joe@hardorange.org>
2014-10-16 04:01:51 -0400

    removed crappy project implementation

--- public/js/controllers/ProjectCtrl.js
+++ public/js/controllers/ProjectCtrl.js
@@ -1,7 +1,3 @@
 angular.module('ProjectCtrl', []).controller('ProjectController', ['$scope', function($scope){
-                              $scope.searchText="";
-
-                              $scope.projects=[{title:"Project 1", description:"This is the description for Project 1", link:"www.google.com"},
-                                                  {title:"Project 2", description:"This is the description for Project 2"}];
 
 }]);
