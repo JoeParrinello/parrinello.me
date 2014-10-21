4bdc33be6817ea8722057be239d5d9c8f5f06667
Joe Parrinello <joe.parrinello@gmail.com>
2014-10-21 19:11:52 -0400

    Projects Page is Changing!

--- public/js/controllers/ProjectCtrl.js
+++ public/js/controllers/ProjectCtrl.js
@@ -1,3 +1,4 @@
 angular.module('ProjectCtrl', []).controller('ProjectController', ['$scope', function($scope){
-
+  $scope.projects = [{title:"Project Name 1", description:'This is Description', imageURL:'/assets/cherries.jpg'},
+  {title:"Project Name 2", description:'This is Description. Lorem Ipsum', imageURL:'/assets/rustCar.jpg'}];
 }]);
