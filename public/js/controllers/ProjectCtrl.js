2e291a985c6f9009b5ad7350bc704f161aae18aa
Joe Parrinello <joe@hardorange.org>
2014-10-22 19:47:13 -0400

    Adding Real Project Images

--- public/js/controllers/ProjectCtrl.js
+++ public/js/controllers/ProjectCtrl.js
@@ -1,6 +1,6 @@
 angular.module('ProjectCtrl', []).controller('ProjectController', ['$scope', function($scope){
-  $scope.projects = [{title:"Project Name 1", description:'This is Description', imageURL:'/assets/cherries.jpg'},
-  {title:"Project Name 2", description:'This is Description. Lorem Ipsum', imageURL:'/assets/rustCar.jpg'}];
+  $scope.projects = [{title:"Project Name 1", description:'This is Description', imageURL:'/assets/WebsiteClip.png'},
+  {title:"Project Name 2", description:'This is Description. Lorem Ipsum', imageURL:'/assets/JavaClip.png'}];
 
 
 }]);
