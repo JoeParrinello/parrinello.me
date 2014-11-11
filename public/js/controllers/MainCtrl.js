d3bdf5a7c976585097e60b4e64657516188a7337
Joe Parrinello <joe@hardorange.org>
2014-11-11 02:02:32 -0500

    Adding references to Friends Pages

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -1,7 +1,11 @@
-angular.module('MainCtrl', []).controller('MainController', ['$rootScope','$scope','$location','$timeout', '$animate', function($rootScope, $scope, $location, $timeout, $animate){
+angular.module('MainCtrl', []).controller('MainController', ['$rootScope','$scope','$location','$timeout', function($rootScope, $scope, $location, $timeout){
 
   $timeout(function(){
     $rootScope.on = true;
   },100);
+
+  $scope.goToPage = function(link){
+    window.open(link,'_newtab');
+  };
 
 }]);
