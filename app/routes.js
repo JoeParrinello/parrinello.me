6482dfde5af329251ee6ab8b7b44940652b0e571
Joe Parrinello <joe@hardorange.org>
2015-02-15 01:16:27 -0500

    New Resume

--- app/routes.js
+++ app/routes.js
@@ -2,7 +2,7 @@ module.exports = function(app){
 
 	app.get('/resume', function(req,res){
 		console.log("sending resume");
-		res.sendFile(app.dirname+'/public/assets/documents/Resume_Joseph_A_Parrinello_11-21-14.pdf');
+		res.sendFile(app.dirname+'/public/assets/documents/Joseph_A_Parrinello_Resume_02-15-2015_Websafe.pdf');
 	});
 
 	app.get('*', function(req,res){
