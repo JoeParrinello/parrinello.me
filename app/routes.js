68743aebd77d82154929622efdb392054fd259a9
Joe Parrinello <joe.parrinello@gmail.com>
2016-05-02 20:12:09 -0400

    updated route to new resume

--- app/routes.js
+++ app/routes.js
@@ -2,7 +2,7 @@ module.exports = function (app) {
 
     app.get('/resume', function (req, res) {
         console.log("sending resume");
-        res.sendFile(app.dirname + '/public/assets/documents/Joseph A Parrinello Resume WebSafe 08-21-15.pdf');
+        res.sendFile(app.dirname + '/public/assets/documents/Joseph A Parrinello Resume WebSafe 05-02-16.pdf');
     });
 
     app.get('*', function (req, res) {
