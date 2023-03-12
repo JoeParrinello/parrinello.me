4069c430001f9cf6f4cec6963e7b1ef7a6269b5d
Joe Parrinello <joe@parrinello.me>
2023-03-12 19:59:03 +0000

    Update project list on personal site

--- js/controllers/ProjectCtrl.js
+++ js/controllers/ProjectCtrl.js
@@ -1,16 +1,22 @@
 angular.module('ProjectCtrl', []).controller('ProjectController', ['$scope', function ($scope) {
     $scope.projects = [
         {
+            title: "BrokerBot",
+            description: "Discord Bot used to get real time stock and cryptocurrency reporting.",
+            imageURL: '/assets/wsbet.gif',
+            link: "https://github.com/JoeParrinello/brokerbot"
+        },
+        {
+            title: "BrokerBot Charty Extension",
+            description: "Cloud function endpoint for BrokerBot used to embed ticker charts for single assets.",
+            imageURL: '/assets/wsbet.gif',
+            link: "https://github.com/JoeParrinello/brokerbot-charty"
+        },
+        {
             title: "OnTap",
             description: "IRC Bot coded in Ruby, with some dabbling in Docker. Used on the #hardorange IRC Channel. Used as a joke \"reward\" system on the channel.",
             imageURL: '/assets/BeerTap.jpg',
             link: "https://github.com/JoeParrinello/OnTap"
-        },
-        {
-            title: "Java LongInteger ADT",
-            description: "Created for CS 435 - Advanced Data Structure & Algorithm Design. Tasked with creating an abstract data type that can model numbers larger than the traditional int or double. The numbers were designed to be compared to numbers, basic arithmetic, and exponentiation. Had to be designed in a way where the number could be implemented with either a Singly Linked List, Doubly Linked List, or Array. Source code can not be put online due to restrictions placed by the instructor.",
-            imageURL: '/assets/JavaClip.png',
-            link: ""
         },
         {
             title: "Java DES Cryptosystem",
@@ -19,29 +25,11 @@ angular.module('ProjectCtrl', []).controller('ProjectController', ['$scope', fun
             link: ""
         },
         {
-            title: "Jimmy Bob's Car-a-palooza",
-            description: "This stemmed from a project for my Introductions to Database Class, CS431. The design specification was for a simple POS unit for a car dealership to handle customer sales. My friend Trevor and I took it a bit farther, and decided to implement some extra technologies for practice. The front end is written as a single page application in AngularJS, where the backend is a Laravel setup, using MySQL database.",
-            imageURL: '/assets/JimmyBob.png',
-            link: 'https://github.com/tsums/CarSalesProject'
-        },
-        {
             title: "JDistributedWork",
             description: 'Programmed using Java Sockets, Threading, and File IO, this was an attempt to recreate a distributed work applications, like BOINC, onto multiple machines. The idea was to make a framework where the input would be a java class file that would have a slice of work to be done, and a method to call when all the slices were complete. Found a wierd bug where I would never be reported the n-1th thread, where n was the number of cores reported on the system.',
             imageURL: '/assets/JavaClip.png',
             link: "https://github.com/HardOrange/JDistributedWork"
         },
-        {
-            title: "Chaotic Clementine",
-            description: "An SMS based event planning app built on Google app Engine. Collaborated with Trevor Summerfield and Matt Lavine.",
-            imageURL: '/assets/AppEngineClip.jpg',
-            link: 'https://github.com/HardOrange/ChaoticClementine'
-        },
-        {
-            title: "CalculatePi",
-            description: "A Java application that runs either with GUI or without to calculate the value of Pi using sums. This leverages concurrency in Java. This was one of the inspirations for JDistributedWork.",
-            imageURL: '/assets/JavaClip.png',
-            link: 'https://github.com/Taevis662/CalculatePi'
-        }
     ];
     $scope.goToPage = function (project) {
         if (project.link !== ""){
